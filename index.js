830ec58a16d0c19f2e4981d1cdc9ea3a6731b6e6
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-24 23:56:34 -0400

    Fix operations with many numbers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let num1 = 0
-let num2 = 0
+let num1 = '0'
+let num2 = '0'
 let operator = ''
 let displayValue = 0
 
@@ -8,9 +8,11 @@ const clearBtn = document.querySelector('#clear').addEventListener('click', clea
 const deleteBtn = document.querySelector('#delete')
 const percentageBtn = document.querySelector('#percentage')
 const addBtn = document.querySelector('#add').addEventListener('click', () => {
-  num1 = Number(display.textContent)
-  display.textContent = '0'
+  // num1 = Number(display.textContent)
+  // display.textContent = '0'
   operator = '+'
+  console.log(num1, num2, operator)
+  operate(num1, num2, operator)
 })
 const subtractBtn = document.querySelector('#subtract').addEventListener('click', () => {
   num1 = Number(display.textContent)
@@ -35,38 +37,58 @@ const dotBtn = document.querySelector('#dot').addEventListener('click', () => {
   populateDisplay('.')
 })
 const one = document.querySelector('#one').addEventListener('click', () => {
+  operator === '' ? num1 += 1 : num2 += 1
+  displayValue = 1
   populateDisplay('1')
 })
 const two = document.querySelector('#two').addEventListener('click', () => {
+  operator === '' ? num1 += 2 : num2 += 2
+  displayValue = 2
   populateDisplay('2')
 })
 const three = document.querySelector('#three').addEventListener('click', () => {
+  operator === '' ? num1 += 3 : num2 += 3
+  displayValue = 3
   populateDisplay('3')
 })
 const four = document.querySelector('#four').addEventListener('click', () => {
+  operator === '' ? num1 += 4 : num2 += 4
+  displayValue = 4
   populateDisplay('4')
 })
 const five = document.querySelector('#five').addEventListener('click', () => {
+  operator === '' ? num1 += 5 : num2 += 5
+  displayValue = 5
   populateDisplay('5')
 })
 const six = document.querySelector('#six').addEventListener('click', () => {
+  operator === '' ? num1 += 6 : num2 += 6
+  displayValue = 6
   populateDisplay('6')
 })
 const seven = document.querySelector('#seven').addEventListener('click', () => {
+  operator === '' ? num1 += 7 : num2 += 7
+  displayValue = 7
   populateDisplay('7')
 })
 const eight = document.querySelector('#eight').addEventListener('click', () => {
+  operator === '' ? num1 += 8 : num2 += 8
+  displayValue = 8
   populateDisplay('8')
 })
 const nine = document.querySelector('#nine').addEventListener('click', () => {
+  operator === '' ? num1 += 9 : num2 += 9
+  displayValue = 9
   populateDisplay('9')
 })
 const zero = document.querySelector('#zero').addEventListener('click', () => {
+  operator === '' ? num1 += 0 : num2 += 0
+  displayValue = 0
   populateDisplay('0')
 })
 
 function add(num1, num2) {
-  return num1 + num2
+  return Number(num1) + Number(num2)
 }
 
 function subtract(num1, num2) {
@@ -85,7 +107,7 @@ function operate(num1, num2, operator) {
   switch (operator) {
     case '+':
       displayValue = add(num1, num2)
-      clear()
+      // clear()
       populateDisplay(displayValue)
       break;
     case '-':
@@ -111,7 +133,7 @@ function operate(num1, num2, operator) {
 function populateDisplay(number) {
   display.textContent === '0'
     ? display.textContent = number
-    : display.textContent += number
+    : display.textContent = displayValue
 }
 
 function clear() {
@@ -119,3 +141,5 @@ function clear() {
   num2 = 0
   display.textContent = '0'
 }
+
+console.log(num1, num2, operator)
